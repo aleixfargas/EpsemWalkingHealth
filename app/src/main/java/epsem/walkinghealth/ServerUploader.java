2abe518475fb7a030650fb76d69a42dcd1b50b79
Aleix <afargas92@gmail.com>
2015-10-21 09:24:27 +0200

    ServerUploader added

---
+++ app/src/main/java/epsem/walkinghealth/ServerUploader.java
@@ -0,0 +1,8 @@
+package epsem.walkinghealth;
+
+public class ServerUploader {
+    protected Void doInBackground(Void... params){
+        //Thread que s'executa en background, accepta el pas de parametres
+
+    }
+}
